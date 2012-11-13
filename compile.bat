12fe78e5782793cd4de64eb6ce3480f87dead807
Cheese <firstact001@gmail.com>
2012-11-13 14:49:04 -0700

    rev:1
    updated compiler to backup scripts incase of catastrophic failure :)

--- compile.bat
+++ compile.bat
@@ -1,25 +1,33 @@
 @echo off
 
+set backupdir=C:\Users\cheese\Desktop\zombiesbackup
 set coddir=D:\Games\Call of Duty 1.1\main
 
-echo Updating...
+echo **** Backing up...
+echo.
+
+cd zombies
+@copy /Y *.* "%backupdir%\zombies"
+cd ../modules
+@copy /Y *.* "%backupdir%\modules"
+cd ../maps/mp/gametypes
+@copy /Y *.* "%backupdir%\maps\mp\gametypes"
+
+echo.
+echo **** Backup complete.
+echo **** Updating...
 echo.
 
 echo ./zombies
-cd zombies
+cd ../../../zombies
 copy /Y *.* "%coddir%\zombies"
-echo.
-
 echo ./modules
 cd ../modules
 copy /Y *.* "%coddir%\modules"
-echo.
-
 echo ./maps/mp/gametypes
 cd ../maps/mp/gametypes
 copy /Y *.* "%coddir%\maps\mp\gametypes"
+
 echo.
-
-echo Completed.
-
+echo **** Update complete.
 pause
